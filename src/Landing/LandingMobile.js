b0ebe5272df62e3328434336482782d0cea0a6fa
JoshImmor <Joshimmor@gmail.com>
2021-06-16 20:37:17 -0400

    mobile view animations

--- src/Landing/LandingMobile.js
+++ src/Landing/LandingMobile.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React,{useRef, useEffect} from 'react';
 import "./LandingMobile.scss";
 import "../Glitch.scss";
+import {TimelineLite} from "gsap";
 
 export default function LandingMobile() {
+    const Tag1 = useRef(null);
+    const line = useRef(null);
+    const tl = new TimelineLite();
+    useEffect(()=>{
+        tl.from(".line",{
+            x:1000,
+            ease:"power2"
+        })
+        .from("#ball",{
+            y:1000,
+            ease:"power2"
+        },"-=.5")
+        .from("#tag1",{
+            opacity:0,
+            ease:"power2"
+        })
+        .from("#tag2",{
+            opacity:0,
+            ease:"power2"
+        })
+        .from("#tag3",{
+            opacity:0,
+            ease:"power2"
+        })
+    },[tl])
     return (
         <div className="Landing-Col">
             <div className="headline">
                 <h1 className="glitch" data-text="
-Software Engineer">Software Engineer</h1>
+Joshua Immordino">Joshua Immordino</h1>
             </div>
             <div className="Links">
-                <span></span>
+                <span className="line"></span>
                 <ul>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                    <li>Github</li>
+                    <li id="tag1">PROJECTS</li>
+                    <li id="tag2">CONTACT</li>
+                    <li id="tag3">GITHUB</li>
                 </ul>
             </div>
             <div className="Navi">
-                     <svg width='60' height='60'>
-                         <circle cx='50' cy='50' r='40'  fill='9D0000' />
+                     <svg id="ball" width='60' height='60'>
+                         <circle cx='20' cy='20' r='20'  fill='#cc0000' />
                     </svg>
             </div>
         </div>
