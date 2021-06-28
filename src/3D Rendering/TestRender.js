6da0f39c34b8d3a04776309d08cd302539c3b737
Joshua Immordino <joshimmor@Gmail.com>
2021-06-28 10:29:36 -0400

    configured camera settings

--- src/3D Rendering/TestRender.js
+++ src/3D Rendering/TestRender.js
@@ -1,13 +1,13 @@
-import React, { Suspense,useRef} from 'react'
+import React, { Suspense,useRef, useState} from 'react'
 import { OrbitControls} from '@react-three/drei';
-import { Canvas, useThree} from '@react-three/fiber';
+import { Canvas, useThree,useFrame} from '@react-three/fiber';
 import Ball from "./Ball";
 
 const Controls = () => {
     const orbitRef = useRef();
     const {camera, gl} = useThree();
 
-
+    camera.position.z = 500;
     return (
         <OrbitControls
             args={[camera, gl.domElement]}
@@ -21,12 +21,22 @@ const Controls = () => {
 export default function TestRender() {
     return (
         <Canvas className="Canvas-Element">
-            <Controls/>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <spotLight position={[10, 10, 10]} />
+            <Controls />
+            <ambientLight  intensity={1.5}/>
+            <pointLight  color="white" intensity={1} position={[100, 100, 100]} />
+            <rectAreaLight
+              width={30}
+              height={30}
+              intensity={10}
+              color="white"
+              position={[50, 50, 5]}
+              lookAt={[0, 0, 0]}
+              penumbra={2}
+              castShadow
+              />
+            <spotLight position={[1000, 1000, 1000]} />
             <Suspense fallback={null}>
-                <Ball/>
+                <Ball scale={.5} position={[0, 0, 0]}/>
             </Suspense>
         </Canvas>
     )
