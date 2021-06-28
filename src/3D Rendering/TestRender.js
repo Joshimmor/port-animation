bc909154b681703b68ee9784db1f5e264842bcaa
Joshua Immordino <joshimmor@Gmail.com>
2021-06-27 23:40:42 -0400

    premire ball

--- src/3D Rendering/TestRender.js
+++ src/3D Rendering/TestRender.js
@@ -1,7 +1,7 @@
-import React, { useRef, useState } from 'react'
-import { OrbitControls } from '@react-three/drei';
+import React, { Suspense,useRef} from 'react'
+import { OrbitControls} from '@react-three/drei';
 import { Canvas, useThree} from '@react-three/fiber';
-
+import Ball from "./Ball";
 
 const Controls = () => {
     const orbitRef = useRef();
@@ -16,42 +16,8 @@ const Controls = () => {
     )
 }
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  //useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-  // Return view, these are regular three.js elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.25 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setActive(true)}
-      onPointerOut={(event) => setActive(false)}
-    >
-      <boxGeometry args={[1.25, 3, .15]} />
-      <meshStandardMaterial  />
-    </mesh>
-  )
-}
-/*
-function Plane() {
-    
-    return (
-      <mesh
-        position={[0,0,0]}
-        rotation={[-Math.PI/2,0,0]}
-      >
-        <planeBufferGeometry attach="geometry" args={[100,100]}/>
-        <meshStandardMaterial attach="material" color="1a281f" />
-      </mesh>
-    )
-  }
-*/
+
+
 export default function TestRender() {
     return (
         <Canvas className="Canvas-Element">
@@ -59,9 +25,9 @@ export default function TestRender() {
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
             <spotLight position={[10, 10, 10]} />
-            <Box position={[0, 1, 0]} />
-            <Box position={[0, 1, 5000]} />
-            <Box position={[0, 1, 10000]} />
+            <Suspense fallback={null}>
+                <Ball/>
+            </Suspense>
         </Canvas>
     )
 }
